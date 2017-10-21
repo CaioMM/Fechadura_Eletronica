3171823edc7784ff68e7c7e209178902231a47d1
Caio <cmmcardoso@gmail.com>
2017-10-20 23:03:42 -0200

    Add files via upload
    codigo com o bluetooth implementado

--- fechadura_eletronica.c
+++ fechadura_eletronica.c
@@ -1,92 +1,77 @@
 #include <EEPROM.h>
-#define confirma 7
-#define muda_senha 6
-#define led_verde 8
+#include <SoftwareSerial.h>
+#include <Stepper.h>
+//comandos recebidos via bluetooth
+#define ok 111
+#define muda_senha 109
+#define fechar_porta 102
+// variáveis utilizadas para identificação dos pinos
+#define led_vermelho 4
+#define led_verde 5
+#define buzzer 6
 
+
+void start(void);
+void recebeDados(void);
+void newPassword(void);
+void fecharPorta(void);
+void esvaziaBuffer(void);
+
+
+const int stepsPerRevolution = 500; // Passos por volya
 int senha[4] = {0, 0, 0, 0};
 int cont = 0;
-char reset_senha = false;
-char nova_senha = false;
+char portaAberta = true;
+char portaFechada = false;
 uint8_t bytereceived = 0;
+SoftwareSerial bluetooth(2, 3); // RX, TX
+Stepper myStepper(stepsPerRevolution, 10, 11, 8, 9); // Declaração do motor
 
 void setup() {
 
 	Serial.begin(9600);
+	bluetooth.begin(9600);
+
 	// inicializando os pinos
-	pinMode(confirma, INPUT);
-	pinMode(muda_senha, INPUT);
 	pinMode(led_verde, OUTPUT);
-	
+	pinMode(led_vermelho, OUTPUT);
+	pinMode(buzzer, OUTPUT);
+
 	// caso não tenha nada gravado na memória à senha padrão será gravada na mesma.
-	if(EEPROM.read(0) == 0){	
+	if(EEPROM.read(0) == 0){  
 		for(int i = 0; i < 4; i++){
 
 			EEPROM.write(i, '0');
 
 		}
+		
 		Serial.println("Senha Padrão Gravada.");
+
+	} else {
+		
+		Serial.println("Senha Existe");
+		
 	}
 
 }
 
 void loop() {
 
-	resetPassword();
+	recebeDados();
 
 	newPassword();
 
 	start();
 
-	if(!digitalRead(muda_senha)){
-
-		nova_senha = true;
-
-	}
+	fecharPorta();
 
 }
 
-
-
-
-
-
-void resetPassword(){
-	if(reset_senha){
-
-		for(int i = 0; i < 4; i++){
-
-			EEPROM.write(i, '0');
-
-		}
-
-	}
-
-	reset_senha = false;
-}
-
-void newPassword(){
-	if(nova_senha){
-		Serial.println("Digite a nova Senha.");
-
-		for(int i = 0; i < 4; i++){
-
-			recebeDadosSerial();
-			EEPROM.write(i, bytereceived);
-			
-		}
-
-		Serial.println("Senha Trocada.");		
-	}
-
-	nova_senha = false;
-}
-
-
-void recebeDadosSerial(){
+void recebeDados(){
 	cont = 0;
 	bytereceived = 0;
-	//aguarda dados na porta serial por 20s
-	while(!Serial.available()){
+	// aguarda dados por 20s
+	while(!bluetooth.available()){
 		if(cont > 200){
 			break;
 		}
@@ -94,29 +79,159 @@ void recebeDadosSerial(){
 		delay(100);
 	}
 
-	//armazena e mostra a opção escolhida
-	if(Serial.available() > 0){
-		bytereceived = Serial.read();
+// armazena o que está no buffer e nos mostra o que foi armazenado
+	if(bluetooth.available() > 0){
+		bytereceived = bluetooth.read();
 		Serial.write(bytereceived);
 		Serial.println();
 	}
 
 }
 
-void start(){
+void newPassword(){
+	if(bytereceived == muda_senha){
+		
+		esvaziaBuffer();
 
-	if(!digitalRead(confirma)){
+		// vetor necessário para verificar se a nova senha foi inserida corretamente
+		int confirma_senha[4] = {0, 0, 0, 0};
 
-		Serial.println("digite sua Senha:");
+		Serial.println("Digite Sua Senha.");
+
+		// buzzer é ligado para avisar o usuário para digitar senha atual
+		tone(buzzer, 1500);
+		delay(1000);
+		notone(buzzer);
 
 		for(int i = 0; i < 4; i++){
-			
-			recebeDadosSerial();
+
+			recebeDados();
 			senha[i] = bytereceived;
 
 		}
 
+		esvaziaBuffer();
 		testeSenha();
+
+		if(cont == 4){
+
+			// caso a senha seja correta o usuário poderá digitar a nova senha
+			digitalWrite(led_verde, HIGH);
+			tone(buzzer, 1500);
+			delay(1000);
+			notone(buzzer);
+
+			Serial.println("Digite a nova Senha.");
+
+			for(int i = 0; i < 4; i++){
+
+				recebeDados();
+				senha[i] = bytereceived;
+			
+			}
+
+			esvaziaBuffer();
+
+			tone(buzzer, 1500);
+			delay(1000);
+			notone(buzzer);
+
+			Serial.println("Confime sua senha.");
+
+			for(int i = 0; i < 4; i++){
+
+				recebeDados();
+				confirma_senha[i] = bytereceived;
+			      
+			}
+
+			cont = 0;
+
+			for(int i = 0; i < 4; i++){
+
+				if(senha[i] == confirma_senha[i]){
+
+					cont++;
+
+				}else{
+
+					Serial.println("Senhas Diferentes, Repita o Processo.");
+					digitalWrite(led_vermelho, HIGH);
+					delay(1000);
+					digitalWrite(led_vermelho, LOW);
+					esvaziaBuffer();
+					break;
+
+				}
+			}
+
+			if(cont == 4){
+				for(int i = 0; i < 4; i++){
+
+					EEPROM.write(i, senha[i]);
+
+				}
+				Serial.println("Senha Trocada.");
+				digitalWrite(led_verde, LOW);
+				esvaziaBuffer();
+			}
+		}
+	}
+}
+
+void fecharPorta(){
+
+	if(bytereceived == fechar_porta){
+
+		if(portaAberta){
+
+			myStepper.step(-498);
+			portaAberta = false;
+			portaFechada = true;
+
+		}
+
+	}
+}
+
+
+
+void start(){
+
+	if(bytereceived == ok){
+
+		esvaziaBuffer();
+		Serial.println("digite sua Senha:");
+		digitalWrite(led_verde, HIGH);
+		digitalWrite(led_vermelho, HIGH);
+
+		for(int i = 0; i < 4; i++){
+
+			recebeDados();
+			senha[i] = bytereceived;
+
+		}
+
+		digitalWrite(led_verde, LOW);
+		digitalWrite(led_vermelho, LOW);
+		esvaziaBuffer();
+		testeSenha();
+
+		if(cont == 4){
+
+			digitalWrite(led_verde, HIGH);
+			delay(1000);
+			digitalWrite(led_verde, LOW);
+
+			if(portaFechada){
+
+				myStepper.step(498);
+				portaAberta = true;
+				portaFechada = false;
+				
+			}
+
+		}
 
 	}
 }
@@ -127,20 +242,28 @@ void testeSenha(){
 	for(int i = 0; i < 4; i++){
 
 		if(senha[i] == EEPROM.read(i)){
+		
 			cont++;
+		
 		}else{
+			
 			Serial.println("Senha Errada");
+			digitalWrite(led_vermelho, HIGH);
+			delay(1000);
+			digitalWrite(led_vermelho, LOW);
 			break;
+		
 		}
 
 	}
 
-	if(cont == 4){
-	
-		Serial.println("Acesso Permitido.");
-		digitalWrite(led_verde, HIGH);
-		delay(500);
-		digitalWrite(led_verde, LOW);
+}
+
+void esvaziaBuffer(){
+
+	//evita que algum digito a mais seja lido evitando erros por falta de atenção
+	while(bluetooth.available() != 0){
+		bluetooth.read();
 	}
-		
+
 }
